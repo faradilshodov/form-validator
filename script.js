7225b7bfe0e058d90cd581b73a2661cacbe9bdba
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2025-05-15 19:55:26 -0400

    Added consoling formdata after submission

--- script.js
+++ script.js
@@ -5,21 +5,69 @@ const messageContainer = document.querySelector(".message-container");
 const message = document.getElementById("message");
 
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm() {
     // Using Constraint Validation API
     isValid = form.checkValidity();
 
     // Style main message for an error
-    message.textContent = "Please, fill out all fields.";
-    message.style.color = "red";
-    messageContainer.style.borderColor = "red";
+    if(!isValid) {
+        message.textContent = "Please fill out all fields.";
+        message.style.color = "red";
+        messageContainer.style.borderColor = "red";
+        return;
+    }
+    
+    // Check if passwords match
+    if(password1El.value === password2El.value) {
+        passwordsMatch = true;
+        password1El.style.borderColor = "green";
+        password2El.style.borderColor = "green";
+    } else {
+        passwordsMatch = false;
+        message.textContent = "Make sure passwords match.";
+        message.style.color = "red";
+        messageContainer.style.borderColor = "red";
+        password1El.style.borderColor = "red";
+        password2El.style.borderColor = "red";
+        return;
+    }
+    
+    // If the form is valid and passwords match
+    if(isValid && passwordsMatch) {
+        message.textContent = "Successfully Registered!";
+        message.style.color = "green";
+        messageContainer.style.borderColor = "green";
+    } else {
+        message.textContent = "Please fill out all fields.";
+        message.style.color = "red";
+        messageContainer.style.borderColor = "red";
+    }
+}
+
+function storeFormData() {
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value,
+    };
+
+    console.log(formData);
 }
 
 function processFormData(e) {
     e.preventDefault();
 
+    // Validate form
     validateForm();
+
+    // Submit data if valid
+    if(isValid && passwordsMatch) {
+        storeFormData();
+    }
 }
 
 // Event listener
